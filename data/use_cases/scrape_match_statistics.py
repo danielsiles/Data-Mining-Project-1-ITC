d164f8b87c7ac8ffb2fc56a1038be323e1cd49df
Daniel Siles <danielsiles9@hotmail.com>
2020-11-27 02:03:23 +0200

    fix: scrape_match_statistics use case

--- data/use_cases/scrape_match_statistics.py
+++ data/use_cases/scrape_match_statistics.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from sqlalchemy.exc import IntegrityError
 
 from data.protocols.db.base_match_repo import BaseMatchRepo
@@ -23,6 +25,10 @@ class ScrapeMatchStatistics(BaseUseCase):
         match = self.match_repository.find_by_id(self.match_id)
         if match is None:
             raise ValueError("Match not found")
+
+        if match.get_date() + timedelta(hours=2) > datetime.now():
+            raise ValueError("The match hasn't happened yet")
+
         try:
             html = self.scraper.scrape(match.get_url().replace("Show", "Live"))
         except Exception:
@@ -33,12 +39,15 @@ class ScrapeMatchStatistics(BaseUseCase):
             raise ValueError("Could not parse HTML")
 
         self._insert_data(match, home_team)
-        self._insert_data(match, away_team)
+        self._insert_data(match, away_team, is_home=False)
 
-    def _insert_data(self, match, team_summary):
+    def _insert_data(self, match, team_summary, is_home=True):
+        team_id = match.get_home_team_id()
+        if not is_home:
+            team_id = match.get_away_team_id()
         try:
             team_summary["match_id"] = match.get_id()
-            team_summary["team_id"] = match._home_team_id
+            team_summary["team_id"] = team_id
             self.match_statistics_repository.create(MatchStatistics(**team_summary))
         except IntegrityError:
             # TODO Decouple DBConnection from use case
