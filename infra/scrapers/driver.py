ec1ab8da8695b2039fb899783800f431bb4ea0ce
Daniel Siles <danielsiles9@hotmail.com>
2020-12-17 01:08:50 +0200

    fix: chrome driver

--- infra/scrapers/driver.py
+++ infra/scrapers/driver.py
@@ -1,7 +1,7 @@
 import sys
 import logging
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 logging.basicConfig(filename='driver_log_file.log',
                     format='%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s',
@@ -19,17 +19,17 @@ class Driver:
         try:
             if len(sys.argv) > 1:
                 if Driver._driver is None:
-                    Driver._driver = webdriver.Firefox(executable_path=sys.argv[1])
+                    Driver._driver = webdriver.Chrome(executable_path=sys.argv[1])
                     logging.info(f"Driver loaded succesfully from {sys.argv[1]}")
                 return Driver._driver
             else:
                 logging.error(f"Driver cannot be found. User entered: {sys.argv[1]}")
                 raise Exception("Please provide a path to the chromedriver")
 
-        except Exception:
+        except Exception as e:
             logging.error(f"Driver cannot be found. User entered: {sys.argv[1]}")
             raise FileNotFoundError("Could not execute webdriver. Make sure you provided the correct path to the "
-                                    "chromedriver")
+                                    "chromedriver",e )
 
     @staticmethod
     def init_driver(driver_path):
@@ -40,10 +40,14 @@ class Driver:
         """
         try:
             if driver_path != "":
-                Driver._driver = webdriver.Chrome(executable_path=driver_path)
+                chrome_options = Options()
+                # chrome_options.add_argument('--headless')
+                chrome_options.add_argument('--no-sandbox')
+                chrome_options.add_argument('--disable-dev-shm-usage')
+                Driver._driver = webdriver.Chrome(executable_path=driver_path, chrome_options=chrome_options)
             else:
                 raise Exception("Please provide a path to the chromedriver")
-        except Exception:
+        except Exception as e:
             logging.error(f"Driver cannot be found. Path entered: {driver_path}")
             raise FileNotFoundError("Could not execute webdriver. Make sure you provided the correct path to the "
-                                    "chromedriver")
+                                    "chromedriver", e)
